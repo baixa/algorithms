9acc7cddb2efb0a4a47b839256b3acce96d1b232
Ivan Maksimchuk <baixa2003@gmail.com>
2023-07-02 14:59:41 +0300

    Add starter packages and files
    - Initialize packages, which will contains solved tasks
    - Add starter Main classes, which describe destination of packages

---
+++ src/main/java/algorithms_java/Main.java
@@ -0,0 +1,7 @@
+package algorithms_java;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Пакет содержит решения задач, описанных в книге Роберта Седжвика \"Алгоритмы на Java\"");
+    }
+}
